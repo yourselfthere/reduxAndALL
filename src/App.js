0fcd3114c3517a58e434c04d823c4cef6212c5b7
Navneet Singh <nsd02rtk@gmail.com>
2024-07-21 01:32:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { useState } from "react";
+const App = () => {
+  const [count, setCount] = useState(0);
+  function incr() {
+    setCount(count + 1);
+  }
+  function decr() {
+    setCount(count - 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={incr}>+</button>
+      <br />
+      <button onClick={decr}>-</button>
     </div>
   );
-}
+};
 
 export default App;
